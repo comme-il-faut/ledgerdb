01983addc21ad1e55c5a835bfc98d36bcfe06d28
U-FLASH\alex <typeractive@users.noreply.github.com>
2016-10-07 05:43:00 -0400

    New subresource /all to get all accounts natural join with account types.

--- webapp/server/src/main/java/ledgerdb/server/resource/AccountResource.java
+++ webapp/server/src/main/java/ledgerdb/server/resource/AccountResource.java
@@ -14,7 +14,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import ledgerdb.server.config.DbConfig;
 
-@Path(value = "/account")
+@Path("/account")
 @PermitAll
 @Produces(MediaType.APPLICATION_JSON)
 public class AccountResource {
@@ -27,11 +27,25 @@ public class AccountResource {
     }
     
     @GET
-    public String getAccountTypes() throws SQLException, JsonProcessingException {
-        
+    @Path("/")
+    public String getAccounts() throws SQLException, JsonProcessingException {
         try (Connection con = dbConfig.getConnection();
                 Statement st = con.createStatement()) {
             ResultSet rs = st.executeQuery("select * from account order by account_id");
+            return JsonUtils.format(rs);
+        }
+    }
+    
+    @GET
+    @Path("/all")
+    public String getAllAccounts() throws SQLException, JsonProcessingException {
+        try (Connection con = dbConfig.getConnection();
+                Statement st = con.createStatement()) {
+            ResultSet rs = st.executeQuery(
+                    "select * from account " +
+                    "natural join account_type " +
+                    "order by account_id"
+            );
             return JsonUtils.format(rs);
         }
     }
