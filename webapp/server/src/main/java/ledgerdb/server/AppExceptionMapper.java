44bf3a22e23b3614a766b81e6bdc8185dba3b1f2
U-FLASH\alex <sashaanderson@users.noreply.github.com>
2016-10-09 10:38:50 -0400

    Log warn about exception.

--- webapp/server/src/main/java/ledgerdb/server/AppExceptionMapper.java
+++ webapp/server/src/main/java/ledgerdb/server/AppExceptionMapper.java
@@ -4,12 +4,18 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Provider
 public class AppExceptionMapper implements ExceptionMapper<Exception> {
-
+    
+    private static final Logger LOG = LoggerFactory.getLogger(AppExceptionMapper.class);
+    
     @Override
     public Response toResponse(Exception e) {
+        LOG.warn("Exception occurred: " + e.toString(), e);
+        
         String message;
         if (e instanceof AppException)
             message = e.getMessage();
