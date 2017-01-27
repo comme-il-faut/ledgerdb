691c4be215d43aeb1d71ff8beab7b6236e006d39
U-FLASH\alex <sashaanderson@users.noreply.github.com>
2017-01-27 06:07:47 -0500

    Call mvn package if target/classes/ are newer than the jar file.

--- server/ledgerdb-server.sh
+++ server/ledgerdb-server.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/bash -ex
 
-java -jar target/ledgerdb-server-1.0-SNAPSHOT.jar server config.yml
+jarfile=target/ledgerdb-server-1.0-SNAPSHOT.jar
+
+if find target/classes/ -type f -name '*.class' -newer $jarfile | grep -q .
+then
+    mvn package
+fi
+
+java -jar $jarfile server config.yml
