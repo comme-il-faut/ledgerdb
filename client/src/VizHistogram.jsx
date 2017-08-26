ebf90d1fd5020f9393572998dd76b9fb539ee702
U-FLASH\alex <sashaanderson@users.noreply.github.com>
2017-08-26 06:16:58 -0400

    WIP

--- client/src/VizHistogram.jsx
+++ client/src/VizHistogram.jsx
@@ -6,6 +6,10 @@ import PromiseContainer from './shared/PromiseContainer';
 import { DATE_FORMAT_MDY, DATE_FORMAT_ISO } from './shared/DateInput';
 import { fetchJSON } from './fetch';
 
+//TODO - resize svn height, when transition multiples/stacked
+//TODO - stacked: add total labels on top of each bar
+//TODO - multiples: hide axis/gridlines/totals, add labels atop each rect
+
 class VizHistogram_Chart extends React.PureComponent {
 
   constructor(props) {
@@ -24,8 +28,12 @@ class VizHistogram_Chart extends React.PureComponent {
   }
 
   componentWillReceiveProps(nextProps) {
-    //TODO ???
-    this.redraw();
+    const changed = Object.keys(nextProps).filter(key => this.props[key] !== nextProps[key]);
+    if (changed.length == 1 && changed.join() == "mode") {
+      this.transition(nextProps.mode);
+    } else {
+      this.redraw();
+    }
   }
 
   clear() {
@@ -36,10 +44,24 @@ class VizHistogram_Chart extends React.PureComponent {
     }
   }
 
+  transition(mode) {
+    try {
+      const svg = d3.select(this.root).select("svg");
+      svg.transition().duration(750)
+        .selectAll("rect")
+        .attr("y", this.t[mode]);
+    } catch (err) {
+      alert(err.toString());
+      console.log(err);
+    }
+  }
+
   redraw() {
     try {
       this.clear();
       const div = d3.select(this.root);
+
+      console.log("VizHistogram_Chart: redraw: mode=" + this.props.mode);
 
       const parseDate = d3.timeParse("%Y-%m"),
             formatDate = d3.timeFormat("%b-%Y"),
@@ -57,9 +79,10 @@ class VizHistogram_Chart extends React.PureComponent {
 
       const x = d3.scaleBand()
         .rangeRound([0, width])
-        .paddingInner(0.2);
+        .paddingInner(0.2)
+        .paddingOuter(0.1);
 
-      const y = d3.scaleLinear()
+      const y1 = d3.scaleLinear()
         .rangeRound([height, 0]);
 
       const color = d3.scaleOrdinal(d3.schemeCategory20);
@@ -91,8 +114,13 @@ class VizHistogram_Chart extends React.PureComponent {
         return d;
       });
 
+      const yOffsets = yKeys.map((accountId, j) => d3.max(data, d => d[accountId]));
+      yOffsets.reduce((a, b, i) => yOffsets[i] = a + b);
+      //debugger;
+
       x.domain(xKeys);
-      y.domain([0, d3.max(data, d => d.total)]).nice();
+      y1.domain([0, d3.max(data, d => d.total)]).nice();
+      //y1.domain([0, yOffsets[yOffsets.length - 1]]).nice();
 
       //TODO handle negative flows
 
@@ -105,11 +133,11 @@ class VizHistogram_Chart extends React.PureComponent {
 
       g.append("g")
         .attr("class", "axis")
-        .call(d3.axisLeft(y));
+        .call(d3.axisLeft(y1));
 
       g.append("g")
         .attr("class", "gridline")
-        .call(d3.axisLeft(y)
+        .call(d3.axisLeft(y1)
           .tickSize(-width)
           .tickFormat(""))
         .call(g =>
@@ -122,17 +150,32 @@ class VizHistogram_Chart extends React.PureComponent {
           g.select(".domain")
             .style("display", "none"));
 
-      g.append("g")
+      const group = g.append("g")
         .selectAll("g")
         .data(d3.stack().keys(yKeys)(data))
         .enter().append("g")
-          .attr("fill", d => color(d.key))
-        .selectAll("rect")
+          .attr("fill", d => color(d.key));
+
+      this.t = {
+        stacked: function(d, i) { return y1(d[1]); },
+        multiples: function(d, i) {
+          const j = this.parentNode.__data__.index;
+          let h = yOffsets[j];
+          if (j > 0) { h -= yOffsets[j - 1]; }
+          //debugger;
+          //return y1(yOffsets[j] + d[1]);
+          return y1(yOffsets[j] - d[0] + d[1] - h) - (j * 3);
+        }
+      };
+
+      group.selectAll("rect")
         .data(d => d)
         .enter().append("rect")
           .attr("x", d => x(d.data.postingMonth))
-          .attr("y", d => y(d[1]))
-          .attr("height", d => y(d[0]) - y(d[1]))
+          //.attr("y", d => y1(d[1]))
+          //.attr("y", this.t.multiples)
+          .attr("y", this.t[this.props.mode])
+          .attr("height", d => y1(d[0]) - y1(d[1]))
           .attr("width", x.bandwidth())
           .append("title").text(function(d, i) {
             const accountId = this.parentNode.parentNode.__data__.key; // series.key
@@ -165,65 +208,80 @@ class VizHistogram_Form extends React.PureComponent {
 
   constructor(props) {
     super(props);
-    this.d1 = this.props.d1;
-    this.d2 = this.props.d2;
     this.state = {
-      selected: "15m", // XXX must agree
-      stale: false
+      date: this.props.date,
+      dateStale: false,
+      mode: this.props.mode
     };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.inputModeRefs = {};
+
+    this.setInputModeRef = this.setInputModeRef.bind(this);
+    this.handleDateChange = this.handleDateChange.bind(this);
+    this.handleRefresh = this.handleRefresh.bind(this);
+    this.handleModeChange = this.handleModeChange.bind(this);
   }
 
-  handleChange(e) {
-    const value = e.target.value;
-    if (value.endsWith("m")) {
-      this.d1 = moment().startOf("month").add(-value.slice(0, -1) + 1, "M");
-      this.d2 = moment().endOf("month");
-    }
-    if (value.startsWith("y")) {
-      this.d1 = moment(value.slice(1) + "-01-01");
-      this.d2 = moment(value.slice(1) + "-12-31");
-    }
-    this.setState({ selected: value, stale: true });
+  componentDidMount() {
+    Object.keys(this.inputModeRefs).forEach(key => {
+      const input = this.inputModeRefs[key];
+      $(input).change(() => this.handleModeChange(key));
+    });
   }
 
-  handleSubmit(e) {
+  // https://github.com/facebook/react/issues/4533
+  setInputModeRef(input) {
+    if (input) {
+      this.inputModeRefs[input.value] = input;
+    }
+  }
+
+  handleDateChange(e) {
+    this.setState({ date: e.target.value, dateStale: true });
+  }
+  handleRefresh(e) {
     e.preventDefault();
-    this.setState({ stale: false });
-    this.props.onSubmit(this.d1, this.d2);
+    this.setState({ dateStale: false });
+    this.props.onDateChange(this.state.date);
+  }
+
+  handleModeChange(mode) {
+    this.props.onModeChange(mode);
   }
 
   render() {
+    //TODO - do not hardcode years, get the dynamically from server
     return (
       <div className="row">
         <div className="col-md-7 col-sm-12">
-          <form className="form-inline" onSubmit={this.handleSubmit}>
-            <select value={this.state.selected} className="form-control" onChange={this.handleChange}>
+          <form className="form-inline" onSubmit={this.handleRefresh}>
+            <select value={this.state.date} className="form-control" onChange={this.handleDateChange}>
               <option value="15m">Past 15 months</option>
               <option value="y2017">Year 2017</option>
               <option value="y2016">Year 2016</option>
             </select>
             &nbsp;
             <div className="form-group">
-              <button type="submit"
-                className={this.state.stale
-                  ? "btn btn-primary"
-                  : "btn btn-default"}
-              >Refresh</button>
+              <button type="submit" className={"btn btn-" + (this.state.dateStale ? "primary" : "default")}>Refresh</button>
             </div>
           </form>
         </div>
         <div className="col-md-5 col-sm-12 text-right">
           <form className="form-inline">
-            <label className="radio-inline">
-              <input type="radio" name="mode" value="multiples"/> Multiples
-            </label>
-            &nbsp;
-            <label className="radio-inline">
-              <input type="radio" name="mode" value="stacked"/> Stacked
-            </label>
+            <div className="btn-group" data-toggle="buttons">
+              <label className={"btn btn-default" + (this.state.mode == "multiples" ? " active" : "")}>
+                <input type="radio" name="mode" autoComplete="off"
+                  value="multiples"
+                  ref={this.setInputModeRef}
+                /> Multiples
+              </label>
+              <label className={"btn btn-default" + (this.state.mode == "stacked" ? " active" : "")}>
+                <input type="radio" name="mode" autoComplete="off"
+                  value="stacked"
+                  ref={this.setInputModeRef}
+                /> Stacked
+              </label>
+            </div>
           </form>
         </div>
       </div>
@@ -232,57 +290,75 @@ class VizHistogram_Form extends React.PureComponent {
 
 }
 
-class VizHistogram extends React.PureComponent {
+class VizHistogram extends React.Component {
 
   constructor(props) {
     super(props);
 
-    const d1 = moment().startOf("month").add(-15 + 1, "M"); //XXX must agree
-    const d2 = moment().endOf("month");
-    this.state = {
-      accounts: null,
-      d1: d1,
-      d2: d2
-    };
+    this.accounts = fetch("api/account", {
+      method: "get",
+      headers: { "Authorization": sessionStorage.token }
+    }).then(fetchJSON)
 
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      date: "15m",
+      mode: "stacked"
+    };
+    this.setDates(this.state.date);
+
+    this.handleDateChange = this.handleDateChange.bind(this);
+    this.handleModeChange = this.handleModeChange.bind(this);
   }
 
-  handleSubmit(d1, d2) {
-    this.setState({ d1: d1, d2: d2 });
-    //alert("d1=" + d1.format(DATE_FORMAT_ISO) + ", d2=" + d2.format(DATE_FORMAT_ISO));
+  componentDidMount() {
+    document.title = "LedgerDB - Reports - Histogram of Expenses";
+  }
+
+  setDates(date) {
+    if (date.endsWith("m")) {
+      this.d1 = moment().startOf("month").add(-date.slice(0, -1) + 1, "M");
+      this.d2 = moment().endOf("month");
+    }
+    if (date.startsWith("y")) {
+      this.d1 = moment(date.slice(1) + "-01-01");
+      this.d2 = moment(date.slice(1) + "-12-31");
+    }
+
+    const url = "api/posting/flows1" +
+      "?d1=" + this.d1.format(DATE_FORMAT_ISO) +
+      "&d2=" + this.d2.format(DATE_FORMAT_ISO);
+    this.histogram = fetch(url, {
+      method: "get",
+      headers: { "Authorization": sessionStorage.token }
+    }).then(fetchJSON);
+  }
+
+  handleDateChange(date) {
+    this.setDates(date);
+    this.setState({ date: date });
+  }
+
+  handleModeChange(mode) {
+    if (this.state.mode != mode) {
+      this.setState({ mode: mode });
+    }
   }
 
   render() {
-    const f2 = url =>
-      fetch(url, {
-        method: "get",
-        headers: { "Authorization": sessionStorage.token }
-      }).then(fetchJSON);
     return (
-      <PromiseContainer
-        accounts={this.state.accounts || f2("api/account")}
-        onResolve={resolvedProps => this.setState(resolvedProps)}
-      >
-        <div>
-          <VizHistogram_Form
-            d1={this.state.d1}
-            d2={this.state.d2}
-            onSubmit={this.handleSubmit}
+      <div>
+        <VizHistogram_Form
+          date={this.state.date}
+          mode={this.state.mode}
+          onDateChange={this.handleDateChange}
+          onModeChange={this.handleModeChange}
+        />
+        <PromiseContainer accounts={this.accounts} histogram={this.histogram}>
+          <VizHistogram_Chart
+            mode={this.state.mode}
           />
-          <PromiseContainer
-            histogram={
-              f2("api/posting/flows1" +
-                "?d1=" + this.state.d1.format(DATE_FORMAT_ISO) +
-                "&d2=" + this.state.d2.format(DATE_FORMAT_ISO))
-            }
-          >
-            <VizHistogram_Chart
-              accounts={this.state.accounts}
-            />
-          </PromiseContainer>
-        </div>
-      </PromiseContainer>
+        </PromiseContainer>
+      </div>
     );
   }
 
